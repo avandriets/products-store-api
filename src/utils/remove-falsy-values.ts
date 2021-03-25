0997b8a2babe9a733f23351997fc61b68a7e1d0e
Alexander Andriets <Alexander.Andriets@acquia.com>
2021-03-25 16:48:51 +0200

    made generic removeFalsyValues

--- src/utils/remove-falsy-values.ts
+++ src/utils/remove-falsy-values.ts
@@ -1,7 +1,7 @@
 /**
  * Provides Remove Falsy Values Utility Function.
  */
-export const removeFalsyValues = (obj: any) => {
+export const removeFalsyValues = <T>(obj: T): Partial<T> => {
 
   return Object.keys(obj).reduce((acc, o) => {
 
